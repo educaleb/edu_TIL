c1819cb5ea20fdaa702b7868fc8b87fe290c5ee4
educaleb <eduhphkcaleb@gmail.com>
2022-11-17 17:25:42 +0900

    day02_1

---
+++ baek1904.py
@@ -0,0 +1,9 @@
+def fibonacci(N):
+    dp = [0]*1000001
+    dp[1], dp[2]=1, 2
+    for i in range(3, N+1):
+        dp[i] = (dp[i-1]+dp[i-2])%15746
+    return dp[i]
+
+N=int(input())
+print(fibonacci(N))
